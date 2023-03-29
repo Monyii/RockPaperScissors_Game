f27bd9805141f82cfdc577a08aa0e6268675db98
Monyii <b.monyi17@gmail.com>
2023-03-29 19:16:26 +0200

    Kezdetleges megvalósítás, html

---
+++ src/main/java/com/monii/rockpaperscissors_game/RockPaperScissorsGameApplication.java
@@ -0,0 +1,13 @@
+package com.monii.rockpaperscissors_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RockPaperScissorsGameApplication.class, args);
+    }
+
+}
